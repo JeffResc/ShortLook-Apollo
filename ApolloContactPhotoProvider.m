a6d0add149a10746a1c2a2daa9b3f1f06e3686ab
Jeff <jeff@jeffresc.dev>
2021-06-13 20:36:25 -0400

    Remove unnecessary braces

--- ApolloContactPhotoProvider.m
+++ ApolloContactPhotoProvider.m
@@ -9,7 +9,7 @@
   NSData *data = [NSData dataWithContentsOfURL: [NSURL URLWithString:[NSString stringWithFormat:@"https://www.reddit.com/user/%@/about.json",username]]];
   NSError* error;
   NSDictionary* json = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:&error];
-  if(error) { return nil; }
+  if(error) return nil;
   NSString *photoURLStr = json[@"data"][@"icon_img"];
   NSURL *photoURL = [NSURL URLWithString:photoURLStr];
   if (!photoURL) return nil;
